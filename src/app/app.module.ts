977809212fb017adaaf5bbd71f54d8732602e211
gilbertKorir <jerikorir@gmail.com>
2022-04-08 22:17:46 +0300

    patch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-<<<<<<< HEAD
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
-=======
-import { AppComponent } from './app.component';
-import { MainQuotesComponent } from './main-quotes/main-quotes.component';
->>>>>>> ffdd058f93d75a366dc563c755f034041532d98c
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     QuotesComponent,
     QuotesFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-=======
-    MainQuotesComponent,
-  
-  ],
-  imports: [
-    BrowserModule
->>>>>>> ffdd058f93d75a366dc563c755f034041532d98c
   ],
   providers: [],
   bootstrap: [AppComponent]
