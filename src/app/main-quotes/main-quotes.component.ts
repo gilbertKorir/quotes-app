ffdd058f93d75a366dc563c755f034041532d98c
gilbertKorir <jerikorir@gmail.com>
2022-04-08 19:11:14 +0300

    refactor the code

--- src/app/main-quotes/main-quotes.component.ts
+++ src/app/main-quotes/main-quotes.component.ts
@@ -9,9 +9,9 @@ import { Quotes } from '../quotes';
 export class MainQuotesComponent implements OnInit {
   title = 'welcome to tech quotes';
   quotes:Quotes[] = [
-    new Quotes (1,'Charles','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
-    new Quotes (2,'Wendy','Technology…','Technology… the knack of so arranging  that we don’t have to experience it.','Max Frisch',new Date(2019,7,14),0,0),
-    new Quotes (3,'Mikey','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2019,6,3),0,0),
+    new Quotes (1,'Jared','Living','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2019,7,12),0,0),
+    new Quotes (2,'Zimmer man','Hardwork','The way to get started is to quit talking and begin doing.','Walt Disney',new Date(2019,7,14),0,0),
+    new Quotes (3,'Dan J','Fact of Life','Life is what happens when you are busy making other plans.','John Lennon',new Date(2019,6,3),0,0),
   ]
   Info = true;
   constructor() { }
